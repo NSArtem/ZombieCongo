90c4184651543e323a466f00c8c839d8f15406f6
Artem Abramov <ar.abramov@gmail.com>
2014-03-30 12:50:22 +0400

    Added zombie spawning

--- ZombieCongo/MainScene.m
+++ ZombieCongo/MainScene.m
@@ -10,6 +10,15 @@
 #import "SKScene+Dimensions.h"
 
 static NSString* const kSpriteBackground = @"background";
+static NSString* const kSpriteZombie1 = @"zombie1";
+
+typedef NS_ENUM(NSUInteger, MainSceneZ)
+{
+    MainSceneZ_MostBottom = 0,
+    MainSceneZ_Background = 10,
+    MainSceneZ_Monsters = 80,
+    MainSceneZ_MostTop = 100
+};
 
 @interface MainScene()
 
@@ -22,6 +31,8 @@ static NSString* const kSpriteBackground = @"background";
 {
     if (self = [super initWithSize:size])
     {
+        [self spawnZombie];
+
         [self addBackground];
     }
     return self;
@@ -32,11 +43,21 @@ static NSString* const kSpriteBackground = @"background";
     
 }
 
+- (void)spawnZombie
+{
+    SKSpriteNode *zombie = [SKSpriteNode spriteNodeWithImageNamed:kSpriteZombie1];
+    zombie.position = CGPointMake(100., 100.);
+    zombie.name = kSpriteZombie1;
+    zombie.zPosition = MainSceneZ_Monsters;
+    [self addChild:zombie];
+}
+
 - (void)addBackground
 {
     SKSpriteNode *background = [SKSpriteNode spriteNodeWithImageNamed:kSpriteBackground];
     background.anchorPoint = CGPointZero;
     background.name = kSpriteBackground;
+    background.zPosition = MainSceneZ_Background;
     [self addChild:background];
     background.position = CGPointZero;
 }
